01904e60368fc0e571d26bf62cbd1f0b24fac5ec
crabhub19 <3xbishal@gmail.com>
2025-02-09 10:55:25 +0600

    a

--- src/components/GoogleAuth.jsx
+++ src/components/GoogleAuth.jsx
@@ -6,19 +6,17 @@ import { useNavigate } from 'react-router-dom';
 import { toast } from 'react-toastify';
 
 const GoogleAuth = () => {
+    const clientId = import.meta.env.VITE_GOOGLE_CLIENT_ID;
     const dispatch = useDispatch()
     const navigate = useNavigate()
     const handleSuccess = async (response) => {
         const token = response.credential;
-        console.log(token);
-        
         if(!token){
             console.error('No token found in the response');
         }
         try {
             const res = await dispatch(googleAuth(token))
             if(googleAuth.fulfilled.match(res)){
-                console.log("successfully logged in");
                 toast.success("successfully logged in");
                 navigate("/");
             }else if (googleAuth.rejected.match(res)){
@@ -30,7 +28,7 @@ const GoogleAuth = () => {
     };
 
     return (
-        <GoogleOAuthProvider clientId="669443405108-8nctfe97fojmp2h0n1oj78024ueuhlri.apps.googleusercontent.com">
+        <GoogleOAuthProvider clientId={clientId}>
             <GoogleLogin
                 onSuccess={handleSuccess}
                 onError={() => console.error('Google login failed')}
